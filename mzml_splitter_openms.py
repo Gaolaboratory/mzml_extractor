961feeccbe15bba2721ec167b69439d63206894b
Yu (Tom) Gao <114178668+Gaolaboratory@users.noreply.github.com>
2024-03-03 22:08:28 -0600

    Update mzml_splitter_openms.py
    reverse to the previous version

--- mzml_splitter_openms.py
+++ mzml_splitter_openms.py
@@ -6,16 +6,16 @@ import time
 import sys
 
 def split_mzml_by_time(mzml_path, rt_ranges, prefix=""):
-    ### rt_ranges should be a list of tuple to indicate the start, end time, and time adjustments
-    ### examples of rt_ranges: [(0,2400, 0),(2400,4800, 2340)]
+    ### rt_ranges should be a list of tuple to indicate the start and end time
+    ### examples of rt_ranges: [(0,2400),(2400,4800)]
     ### the splited mzml files will be saved into the same path as orginal mzml file
     start_time = time.time()
     mzml_file = mzml_path
     rt_ranges = rt_ranges
     if prefix == "" or prefix==None:
         prefix = os.path.basename(mzml_file).replace(".mzML", "")
-    for i, (start, stop, time_adjust) in enumerate(rt_ranges):
-        output_file = os.path.join(os.path.dirname(mzml_file), f"%s_{i + 1}_rt_{start}-{stop}-{time_adjust}.mzML"%prefix)
+    for i, (start, stop) in enumerate(rt_ranges):
+        output_file = os.path.join(os.path.dirname(mzml_file), f"%s_{i + 1}_rt_{start}-{stop}.mzML"%prefix)
         exp = ms.MSExperiment()
         ms.MzMLFile().load(mzml_file, exp)
         print("Reading mzml file from %s to %s"%(start, stop), time.time() - start_time)
@@ -23,7 +23,6 @@ def split_mzml_by_time(mzml_path, rt_ranges, prefix=""):
         j = 0
         for spectrum in exp.getSpectra():
             if start <= spectrum.getRT() <= stop:
-                spectrum.setRT(spec_rt-float(time_adjust))
                 spectrum.setNativeID(f"scan={j+1}")
                 filtered_exp.addSpectrum(spectrum)
                 j+=1
@@ -34,11 +33,11 @@ def split_mzml_by_time(mzml_path, rt_ranges, prefix=""):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='python3 mzml_splitter_openms.py',
-        description='Example: python mzml_splitter_openms.py -i input.mzml -s [(0,600, 0),(600,1200, 540),(1200,1800, 1080)]',
+        description='Example: python mzml_splitter_openms.py -i input.mzml -s [(0,600),(600,1200),(1200,1800)]',
         epilog='Gao lab mzml splitter 1.0, https://lab.gy, contact: yugao@uic.edu')
     parser.add_argument('-i', '--input_file')
     parser.add_argument('-p', '--prefix')
-    parser.add_argument('-s', '--split_time_array', help='split time array in the format of [(0, 2400, 0),(2400, 4800, 2340)], numbers are in seconds')
+    parser.add_argument('-s', '--split_time_array', help='split time array in the format of [(0, 2400),(2400, 4800)], numbers are in seconds')
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
