1f245c77970c620d4fab6e0e480105ed9f5fd983
Gaolaboratory <yugao@uic.edu>
2024-05-04 21:31:59 -0500

    minor change

--- mzml_splitter_openms.py
+++ mzml_splitter_openms.py
@@ -12,13 +12,13 @@ def split_mzml_by_time(mzml_path, rt_ranges, prefix=""):
     start_time = time.time()
     mzml_file = mzml_path
     rt_ranges = rt_ranges
-    if prefix == "":
-        prefix = os.path.basename(mzml_file)
+    if prefix == "" or prefix==None:
+        prefix = os.path.basename(mzml_file).replace(".mzML", "")
     for i, (start, stop) in enumerate(rt_ranges):
         output_file = os.path.join(os.path.dirname(mzml_file), f"%s_{i + 1}_rt_{start}-{stop}.mzML"%prefix)
         exp = ms.MSExperiment()
         ms.MzMLFile().load(mzml_file, exp)
-        print(time.time() - start_time)
+        print("Reading mzml file from %s to %s"%(start, stop), time.time() - start_time)
         filtered_exp = ms.MSExperiment()
         j = 0
         for spectrum in exp.getSpectra():
@@ -27,7 +27,7 @@ def split_mzml_by_time(mzml_path, rt_ranges, prefix=""):
                 filtered_exp.addSpectrum(spectrum)
                 j+=1
         ms.MzMLFile().store(output_file, filtered_exp)
-        print(i,j, (start, stop), time.time() - start_time)
+        print("Total spectrum number", j, (start, stop), time.time() - start_time, output_file)
 
 
 if __name__ == '__main__':
@@ -36,6 +36,7 @@ if __name__ == '__main__':
         description='Example: python mzml_splitter_openms.py -i input.mzml -s [(0,600),(600,1200),(1200,1800)]',
         epilog='Gao lab mzml splitter 1.0, https://lab.gy, contact: yugao@uic.edu')
     parser.add_argument('-i', '--input_file')
+    parser.add_argument('-p', '--prefix')
     parser.add_argument('-s', '--split_time_array', help='split time array in the format of [(0, 2400),(2400, 4800)], numbers are in seconds')
     args = parser.parse_args()
 
@@ -45,4 +46,4 @@ if __name__ == '__main__':
 
     print("Extracting %s ..." % (args.input_file))
     range_list = [tuple(map(float, i.split(','))) for i in re.findall(r"\((.*?)\)", args.split_time_array)]
-    split_mzml_by_time(args.input_file, range_list)
+    split_mzml_by_time(args.input_file, range_list, prefix = args.prefix)
